41c7888d606631cb55e8c38e52957d2b5fc28d6d
Alejandro Sánchez <alejandrosanchezlirola@gmail.com>
2024-09-05 22:44:56 +0200

    add calc_int_simp function

--- calc_int_comp.py
+++ calc_int_comp.py
@@ -30,7 +30,33 @@ def calc_int_simp(cap_ini, contrib, interes, anualidades):
     """
     Calcula el rendimiento con una capitalización anual
     """
-    pass
+    contrib_anual = contrib * 12
+    cap = cap_ini + contrib_anual # Se suma la aportación inicial y la aportación del primer año
+    total_aportado = cap_ini + contrib_anual
+    total_beneficios = 0
+
+    print("\n")
+
+    for i in range(anualidades):
+        print(f"Capital base del año {i+1}: {cap:.2f}")
+
+        # Calcular los intereses después de la aportación anual
+        interes_ganado = cap * interes
+        cap += interes_ganado
+        total_beneficios += interes_ganado
+
+        print(f"Interés ganado en el año {i+1}: {interes_ganado:.2f}")
+        print(f"Capital después de intereses del año {i+1}: {cap:.2f}\n")
+
+        # Añadir la aportación anual para el siguiente año, excepto en el último año
+        if i < anualidades - 1:
+            cap += contrib
+            total_aportado += contrib
+
+    print(f"Total aportado: {total_aportado:.2f}")
+    print(f"Total beneficios generados por intereses: {total_beneficios:.2f}")
+    print(f"Media de interés anual: {total_beneficios/anualidades:.2f}")
+    print(f"Capital total después de {anualidades} años: {cap:.2f}")
 
 
 def calc_int_comp(cap_ini, contrib, interes, anualidades):
